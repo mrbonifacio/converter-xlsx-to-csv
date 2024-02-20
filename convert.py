ca47b57d4ae3bb3011fc371971bc5194bfabde75
Vinícius Bonifácio Pereira <mrbonifaciobr@gmail.com>
2024-02-20 13:11:10 -0300

    Update

--- convert.py
+++ convert.py
@@ -5,24 +5,26 @@ import warnings
 # Desativar temporariamente os avisos relacionados ao openpyxl
 warnings.simplefilter("ignore", UserWarning)
 
-def converte_para_csv():
-    # Definir o diretório onde estão os arquivos .xlsx
-    diretorio = f'C:/Users/{os.getlogin()}/temp/excel/'
-    destino = 'C:\\Users\\vinicius.pereira\\Documents\\GitHub\\unificador-csv\\baseCarteira\\'
+def converte_para_csv(diretorio_origem, diretorio_destino):
+    # Listar todos os arquivos .xlsx no diretório de origem
+    arquivos_xlsx = [arquivo for arquivo in os.listdir(diretorio_origem) if arquivo.endswith('.xlsx')]
 
-    # Listar todos os arquivos .xlsx no diretório
-    arquivos_xlsx = [arquivo for arquivo in os.listdir(
-        diretorio) if arquivo.endswith('.xlsx') and 'HISTORICO_AGENDAMENTO' in arquivo]
-
-    # Converter cada arquivo .xlsx para .csv separado por tabulação
+    # Converter cada arquivo .xlsx para .csv
     for arquivo in arquivos_xlsx:
         # Ler o arquivo .xlsx
-        df = pd.read_excel(os.path.join(diretorio, arquivo), engine='openpyxl')
+        df = pd.read_excel(os.path.join(diretorio_origem, arquivo), engine='openpyxl')
 
         # Definir o caminho e nome do arquivo .csv
-        caminho_csv = os.path.join(destino, f"{arquivo.split('.')[0]}.csv")
+        caminho_csv = os.path.join(diretorio_destino, f"{arquivo.split('.')[0]}.csv")
 
         # Salvar o arquivo .csv separado por tabulação
-        df.to_csv(caminho_csv, sep='\t', encoding='utf-16', index=False)
+        df.to_csv(caminho_csv, sep='\t', index=False)
 
-converte_para_csv()
+# Diretório onde estão os arquivos .xlsx
+diretorio_origem = 'C:/Users/vinicius.pereira/Documents/GitHub/converter-xlsx-to-csv/excel/'
+
+# Diretório onde serão salvos os arquivos .csv
+diretorio_destino = 'C:/Users/vinicius.pereira/Documents/GitHub/converter-xlsx-to-csv/csv/'
+
+# Converter arquivos .xlsx para .csv separados por tabulação
+converte_para_csv(diretorio_origem, diretorio_destino)
